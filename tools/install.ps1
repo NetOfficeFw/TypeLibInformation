b5ebe0165d3c5731754df57681f27e3b26958de7
Jozef Izso <jozef.izso@gmail.com>
2014-11-15 14:36:24 +0100

    Fixed the name of referenced interop library.

--- tools/install.ps1
+++ tools/install.ps1
@@ -2,4 +2,4 @@ param($installPath, $toolsPath, $package, $project)
 
 RegSvrEx.exe /c Join-Path $toolsPath '\TlbInf32.dll'
 
-$project.Object.References | Where-Object { $_.Name -eq "TypeLibInformation32" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
+$project.Object.References | Where-Object { $_.Name -eq "TlbInf32.Interop" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
