0534e15df86de2bf58bfa36c168e067ea9bc5d9d
Jozef Izso <jozef.izso@gmail.com>
2014-11-16 00:19:32 +0100

    Removed the OAPERUSERTLIBREG=1 environment variable because RegSvrEx utility is fixed and can register COM servers to HKCU on modern systems.

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
 
-$env:OAPERUSERTLIBREG=1
 $RegSvrEx=Join-Path $toolsPath "RegSvrEx.exe"
 $TlbInf32=Join-Path $toolsPath "TlbInf32.dll"
 & $RegSvrEx /c $TlbInf32
