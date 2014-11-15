341c4c7743ce0482c80adfd95138ff69ef3fedb9
Jozef Izso <jozef.izso@gmail.com>
2014-11-15 15:18:40 +0100

    Fixed Powershell scripts. Fixed registration to HKCU on Vista SP1 and newer.

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,5 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-RegSvrEx.exe /c Join-Path $toolsPath '\TlbInf32.dll'
+$env:OAPERUSERTLIBREG=1
+& ".\RegSvrEx.exe" /c Join-Path $toolsPath 'TlbInf32.dll'
 
 $project.Object.References | Where-Object { $_.Name -eq "TlbInf32.Interop" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
--- tools/uninstall.ps1
+++ tools/uninstall.ps1
@@ -1,3 +1,4 @@
 param($installPath, $toolsPath, $package, $project)
 
-RegSvrEx.exe /c /u Join-Path $toolsPath '\TlbInf32.dll'
+$env:OAPERUSERTLIBREG=1
+& ".\RegSvrEx.exe" /c /u Join-Path $toolsPath 'TlbInf32.dll'
