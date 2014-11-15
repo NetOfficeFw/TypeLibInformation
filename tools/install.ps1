ee2e98d4f29270cb0a64eedf67f133d098d96139
Jozef Izso <jozef.izso@gmail.com>
2014-11-15 15:39:32 +0100

    Powershell scripts call RegSvrEx.exe correctly now.

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,6 +1,8 @@
 param($installPath, $toolsPath, $package, $project)
 
 $env:OAPERUSERTLIBREG=1
-& ".\RegSvrEx.exe" /c Join-Path $toolsPath 'TlbInf32.dll'
+$RegSvrEx=Join-Path $toolsPath "RegSvrEx.exe"
+$TlbInf32=Join-Path $toolsPath "TlbInf32.dll"
+& $RegSvrEx /c $TlbInf32
 
 $project.Object.References | Where-Object { $_.Name -eq "TlbInf32.Interop" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
--- tools/uninstall.ps1
+++ tools/uninstall.ps1
@@ -1,4 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 $env:OAPERUSERTLIBREG=1
-& ".\RegSvrEx.exe" /c /u Join-Path $toolsPath 'TlbInf32.dll'
+$RegSvrEx=Join-Path $toolsPath "RegSvrEx.exe"
+$TlbInf32=Join-Path $toolsPath "TlbInf32.dll"
+& $RegSvrEx /c /u $TlbInf32
